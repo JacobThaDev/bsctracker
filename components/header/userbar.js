618f44116f858d889a8071affaa9bb23a4975f13
OG KingFox <rune.evo2012@gmail.com>
2021-12-17 07:08:20 -0600

    reverted text change...

--- components/header/userbar.js
+++ components/header/userbar.js
@@ -50,7 +50,7 @@ export default function Userbar() {
                                     target="_blank"
                                     rel="nofollow noopener"
                                     className="btn btn-link text-white px-4">
-                                    Updates
+                                    Update Log
                                 </a>
                                 <OverlayTrigger
                                     placement="left"
