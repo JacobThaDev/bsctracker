63237f7d49b09e6adb315244a06b910e70555c3e
OG KingFox <rune.evo2012@gmail.com>
2021-12-17 06:01:16 -0600

    Started update page

--- pages/updates.js
+++ pages/updates.js
@@ -1,5 +1,8 @@
 import React, { useEffect, Fragment, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import Footer from '../components/global/footer';
+import PageHead from '../components/head';
+import Userbar from '../components/header/userbar';
 
 export default function Updates({...props}) {
 
@@ -11,7 +14,23 @@ export default function Updates({...props}) {
 
     return(
         <Fragment>
+            <PageHead title="Update Log" />
+
+            <div className="d-flex align-items-center login-box flex-column">
+                <div className="login-box-inner" style={{maxWidth: 700}}>
+                    <h5>Update History</h5>
+                    <Card className="text-center">
+                        <Card.Body>
+
+                        </Card.Body>
+                    </Card>
+                    <Footer/>
+                </div>
+            </div>
+
             
+
+
         </Fragment>);
     
 
