1f89b5c8faf29f2db87db03e02a2bc534dcf0f29
OG KingFox <rune.evo2012@gmail.com>
2021-12-22 16:14:36 -0600

    fixed enter address not working

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Card, Button, Form, FormGroup, FormControl } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Button, Col, Row, Form, FormGroup, FormControl } from 'react-bootstrap';
 
 import PageHead from '../components/head';
 import Footer from '../components/global/footer';
@@ -10,9 +10,17 @@ import Cookies from 'js-cookie';
 
 export default function Homepage() {
 
+    const [showForm, setShowForm] = useState(false);
+
     useEffect(() => {
         const web3Modal = new Web3Modal({});
         let connect = document.getElementById("connect");
+        let manual  = document.getElementById("manual");
+        
+        manual.addEventListener("click", async function(e) {
+            e.preventDefault();
+            setShowForm(true);
+        });
 
         connect.addEventListener("click", async function(e) {
             e.preventDefault();
@@ -37,6 +45,26 @@ export default function Homepage() {
         });
     }, []);
 
+    const submitForm = (event) => {
+        event.preventDefault();
+        let formData = new FormData(event.target);
+        let address  = formData.get("address");
+        let parts    = address.split("x");
+        let field    = document.getElementById("address");
+
+        field.classList.remove("border-danger");
+
+        if (parts.length != 2 
+                || parts[0] !== '0' 
+                || parts[1].length < 40 
+                || !/^([A-Za-z0-9]+)+$/.test(parts[1])) {
+            field.classList.add("border-danger");
+        }
+
+        Cookies.set("wallet", address, { expires: 7 });
+        window.location = "/track";
+    };
+
     return (<>
         <PageHead title="Home" />
 
@@ -51,7 +79,21 @@ export default function Homepage() {
                             Connect your wallet to track your balance and 
                             reflections for SafeMoon v2
                         </p>
-
+                        {showForm ? 
+                            <Form autoComplete='off' id="address_form" onSubmit={submitForm}>
+                                <FormGroup className="mb-3 text-start">
+                                    <small className="text-muted">
+                                        Address
+                                    </small>
+                                    <FormControl name="address" id="address"/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Button type="submit" className="btn btn-success w-100">
+                                        Continue
+                                    </Button>
+                                </FormGroup>
+                            </Form>
+                        : 
                         <div className="d-flex align-items-center">
                             <div className="w-100">
                                 <Button id="connect" variant="primary w-100">
@@ -67,6 +109,7 @@ export default function Homepage() {
                                 </Button>
                             </div>
                         </div>
+                        }
                     </Card.Body>
                 </Card>
 
--- pages/txns.js
+++ pages/txns.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 
 import * as Functions from '../functions';
-import PriceChart from '../components/tracker/price_chart';
 import Layout from '../components/global/layout';
 import Cookies from 'js-cookie';
 import Transactions from '../components/tracker/transactions';
